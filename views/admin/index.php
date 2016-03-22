74dbe996d5466c7bfd4cb2060af8397e519542b2
Fedor <fedoryurkov@gmail.com>
2016-03-22 13:45:11 +0300

    base

---
+++ views/admin/index.php
@@ -0,0 +1,11 @@
+<?php
+include (ROOT.DS.'views'.DS.'admin'.DS.'layouts-admin'.DS.'header-admin.php');
+?>
+
+<main>					 
+	<h2>Главная страница административной панели!</h2>
+</main>	
+
+<?php
+include (ROOT.DS.'views'.DS.'admin'.DS.'layouts-admin'.DS.'footer-admin.php');
+?>
