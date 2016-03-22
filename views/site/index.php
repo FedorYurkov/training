0a70a6a172732cd89f79f1f4df65cc58795f6dcd
Fedor <fedoryurkov@gmail.com>
2016-03-22 13:46:22 +0300

    base

---
+++ views/site/index.php
@@ -0,0 +1,10 @@
+<?php
+include (ROOT.DS.'views'.DS.'site'.DS.'layouts'.DS.'header.php');
+include (ROOT.DS.'views'.DS.'site'.DS.'layouts'.DS.'nav.php');
+include (ROOT.DS.'views'.DS.'site'.DS.'layouts'.DS.'menu.php');
+include (ROOT.DS.'views'.DS.'site'.DS.'layouts'.DS.'aside.php');
+
+include (ROOT.DS.'views'.DS.'site'.DS.$viewTpl.'.php');
+
+include (ROOT.DS.'views'.DS.'site'.DS.'layouts'.DS.'footer.php')
+?>
