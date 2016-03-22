edb4c5e05e98b2e237eb69193498fb40c3e13631
Fedor <fedoryurkov@gmail.com>
2016-03-22 13:45:33 +0300

    base

---
+++ views/admin/layouts-admin/footer-admin.php
@@ -0,0 +1,19 @@
+		<footer>
+			<p>&copy; Все права защищены. Федор Юрков 2016г.</p>
+		</footer>
+	</div>
+
+		<script src="/templates/js/jquery.js"></script>
+		<script>
+			$(document).ready(function(){
+				$(".add-to-cart").click(function () {
+					var id = $(this).attr("data-id");
+					$.post("/cart/add/"+id, {}, function (data) {
+						$("#cart-count").html(data);
+					});
+					return false;
+				});
+			});
+		</script>
+	</body>
+</html>
---
+++ views/admin/layouts-admin/header-admin.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Админ панель сайта podvohShop</title>
+		<link rel="stylesheet" type="text/css" href="/templates/css/style-tpl.css">
+	</head>
+
+	<body>
+		<div id="container">
+			<header>
+				
+				<div class="logo">
+					<p><a href="/">ЛОГОТИП ФИРМЫ</a><br>
+					слоган </p>
+				</div>
+
+				<div class="adress">Наш адрес:<br>
+						г. Город, ул. Улица, д. ХХ<br>
+						тел: +7 (978) xxx-xx-xx<br>
+						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;(0000) xx-xx-xx
+				</div>
+
+			</header>
+
+			<nav>
+				<ul class="main-menu">
+					<li><a href="/admin">Админпанель</a></li>
+					<li><a href="/">На сайт</a></li>
+					<li><a href="/user/logout">Выход</a></li>
+				</ul>
+			</nav>
+
+			<div id="menu">
+				<ul>
+					<li><a href="/admin/product">Товары</a></li>
+					<li><a href="/admin/category">Категории</a></li>
+					<li><a href="/admin/order">Заказы</a></li>
+					<li><a href="/admin/users">Пользователи</a></li>
+				</ul>
+				
+			</div>
+
+			<aside>				
+			</aside>
